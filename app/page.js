b251d5a8dd8badc7fbe9de29b91e615da161d7b6
GeorgeKlemperer <george@klemperer.co.uk>
2023-10-25 12:02:44 +0100

    landing page done

--- app/page.js
+++ app/page.js
@@ -25,12 +25,21 @@ export default function Home() {
           />
         </div>
 
-        <Link
-          className='mx-auto p-4 text-black bg-white rounded-lg text-center'
-          href='/listings/'
-        >
-          See the collection
-        </Link>
+        <div className='mx-auto'>
+          <p className='mb-5 text-white px-10'>new arrivals</p>
+          <p className='mb-10 text-white px-10 text-justify text-xl max-w-28rem'>
+            Handpicked treasures from every corner of the world, especially
+            selected for the discerning collector. Stocks limited.
+          </p>
+          <div className='text-center'>
+            <Link
+              className='p-4 text-black bg-white rounded-lg text-center'
+              href='/listings/'
+            >
+              See the collection
+            </Link>
+          </div>
+        </div>
       </div>
     </main>
   )
